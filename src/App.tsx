0d0007041968b9500e98685fb06dbeca694b49d9
Surtr Blaze <codesurtr@gmail.com>
2024-02-14 07:48:45 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import mudrock from './assets/Mudrock_by_YokiDayo.gif';
+
+function App() {
+
+  return (
+    <main>
+      <div className="content">
+        <img src={mudrock} alt="Mudrock run" width="250px"/>
+        <h1>Site under construction</h1>
+      </div>
+    </main>
+
+  )
+}
+
+export default App
