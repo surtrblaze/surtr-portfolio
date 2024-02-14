2699887a6c80fd278f5f89615fa366bf1427c593
Surtr <125942655+surtrblaze@users.noreply.github.com>
2024-02-14 08:19:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://surtrblaze.github.io/surtr-portfolio'
+  base:'https://www.surtrblaze.tech'
 })
